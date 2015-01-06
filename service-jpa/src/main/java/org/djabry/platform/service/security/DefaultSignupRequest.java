16ddbc2bd1908e22f9abc3f800b8e68e27cb46e0
djabry <djabry@gmail.com>
2015-01-06 02:27:13 +0000

    added tests for common authentication operations

---
+++ service-jpa/src/main/java/org/djabry/platform/service/security/DefaultSignupRequest.java
@@ -0,0 +1,32 @@
+package org.djabry.platform.service.security;
+
+import lombok.Data;
+import org.djabry.platform.domain.api.SignUpRequest;
+import org.djabry.platform.domain.api.annotations.Password;
+import org.djabry.platform.domain.api.annotations.Username;
+import org.hibernate.validator.constraints.Email;
+
+/**
+ * Created by djabry on 05/01/15.
+ */
+
+@Data
+public class DefaultSignUpRequest implements SignUpRequest {
+
+    /**
+     * The requested username
+     */
+    @Username
+    String username;
+
+    /**
+     * The requested password
+     */
+    @Password
+    String password;
+    /**
+     * The requested email
+     */
+    @Email
+    String email;
+}
